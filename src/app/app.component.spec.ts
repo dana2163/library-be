8648cd04aae65cecade159e0b0e135fa8aa2d680
Dana  Stepanenko <danastepanenko20@gmail.com>
2023-02-12 21:37:58 +0100

    uloha 1 nefunguju  checkboxy inak funkcne

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,5 +1,5 @@
 import { TestBed } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import { AppComponent } from '../../../WeTe3/src/app/app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'WeTe3-fe'`, () => {
+  it(`should have as title 'WeTe3'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('WeTe3-fe');
+    expect(app.title).toEqual('WeTe3');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('WeTe3-fe app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('WeTe3 app is running!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'WeTe3-fe';
+  decimalNumber: number;
+  binaryArray: number[] = [0, 0, 0, 0, 0, 0, 0, 0];
+
+  constructor() {
+    this.decimalNumber = 0;
+  }
+
+  updateBinaryArray() {
+    this.binaryArray = this.decimalNumber
+      .toString(2)
+      .padStart(8, '0')
+      .split('')
+      .map(char => +char);
+  }
+
+  updateDecimalNumber() {
+    this.decimalNumber = parseInt(this.binaryArray.join(''), 2);
+  }
 }
+import { Component } from '@angular/core';
